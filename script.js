70e7f8d8df9e0df000b18df0fcfac7b132349bd5
Vanessa Omoragbon <vanessa.omoragbon@outlook.com>
2025-10-21 14:59:14 +0100

    hc

--- script.js
+++ script.js
@@ -27,22 +27,39 @@ const closeMenu = document.querySelector(".close-menu");
 const menuIcon = document.querySelector(".menu-icon");
 const navS = document.querySelectorAll(".mobile-menu nav a");
 
-menuIcon.addEventListener("click", () => {
+const toggleSidebar = () => {
   overLay.classList.toggle("open");
   document.body.classList.toggle("no-scroll");
   mobileMenu.classList.toggle("open");
-});
+};
 
-closeMenu.addEventListener("click", () => {
-  overLay.classList.toggle("open");
-  document.body.classList.toggle("no-scroll");
-  mobileMenu.classList.toggle("open");
-});
+menuIcon.addEventListener("click", toggleSidebar);
+
+closeMenu.addEventListener("click", toggleSidebar);
+
+overLay.addEventListener("click", toggleSidebar);
 
 navS.forEach((nav) => {
-  nav.addEventListener("click", () => {
-    mobileMenu.classList.toggle("open");
-    overLay.classList.toggle("open");
-    document.body.classList.toggle("no-scroll");
-  });
+  nav.addEventListener("click", toggleSidebar);
 });
+
+const images = [
+  "images/pic1.jpg",
+  "images/pic2.jpg",
+  "images/pic3.jpg",
+  "images/pic4.jpg",
+];
+
+let index = 0;
+const aboutPicture = document.querySelector(".about-image img");
+
+const changeImage = () => {
+  aboutPicture.style.opacity = 0;
+  setTimeout(() => {
+    index = (index + 1) % images.length;
+    profilePic.src = images[index];
+    aboutPicture.style.opacity = 1;
+  }, 500);
+};
+
+setInterval(changeImage, 30000);
